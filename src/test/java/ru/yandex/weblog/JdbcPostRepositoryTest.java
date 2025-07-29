048fc7699c6791cfbe87c023b540726a363cd0cd
elizavetavolkova <lizap@bk.ru>
2025-07-29 13:17:56 +0300

    migrated to Spring Boot

--- src/test/java/ru/yandex/weblog/JdbcPostRepositoryTest.java
+++ src/test/java/ru/yandex/weblog/JdbcPostRepositoryTest.java
@@ -1,44 +1,28 @@
 package ru.yandex.weblog;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import ru.yandex.weblog.configuration.DataSourceConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import ru.yandex.weblog.model.Post;
 import ru.yandex.weblog.repository.PostRepository;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringJUnitConfig(classes = {DataSourceConfiguration.class, JdbcPostRepositoryTest.class})
-@ComponentScan(basePackages = "ru.yandex.weblog.repository")
+@SpringBootTest
+@Sql(scripts = "classpath:test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @TestPropertySource(locations = "classpath:test-application.properties")
 public class JdbcPostRepositoryTest {
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
 
     @Autowired
     private PostRepository postRepository;
 
     @Value("${test-sql.path}")
     private String testSqlPath;
-
-    @BeforeEach
-    void setUp() throws IOException {
-        // Очистка и заполнение тестовых данных в базе
-        String sql = Files.readString(Paths.get(testSqlPath));
-        jdbcTemplate.execute(sql);
-    }
 
     @Test
     void save_shouldAddUserToDatabase() {
--- src/test/java/ru/yandex/weblog/PostControllerIntegrationTest.java
+++ src/test/java/ru/yandex/weblog/PostControllerIntegrationTest.java
@@ -1,55 +1,30 @@
 package ru.yandex.weblog;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import ru.yandex.weblog.configuration.DataSourceConfiguration;
-
-import javax.sql.DataSource;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringJUnitConfig(classes = {DataSourceConfiguration.class, WebConfiguration.class})
-@WebAppConfiguration
+@SpringBootTest
+@AutoConfigureMockMvc
+@Sql(scripts = "classpath:test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @TestPropertySource(locations = "classpath:test-application.properties")
 public class PostControllerIntegrationTest {
 
     @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    DataSource dataSource;
-
     private MockMvc mockMvc;
 
     @Value("${test-sql.path}")
     private String testSqlPath;
-
-    @BeforeEach
-    void setUp() throws IOException {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-        // Очистка и заполнение тестовых данных в базе
-        String sql = Files.readString(Paths.get(testSqlPath));
-        jdbcTemplate.execute(sql);
-    }
 
     @Test
     void getPosts_shouldReturnHtmlWithPosts() throws Exception {
